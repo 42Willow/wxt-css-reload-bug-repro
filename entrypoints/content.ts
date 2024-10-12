0d7ac268982812970866f0f04d4acd27f68f4c5b
willow <42willow@pm.me>
2024-10-12 15:43:37 +1100

    docs: oops not related to tailwind

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,7 +1,6 @@
 import "./some.css";
 // if the above line isn't included, then a change in that file will not trigger a reload
 // if the above line is included, then a change in that file will trigger a reload, but not update the contents of the css file
-// this occurs with or without using tailwind css
 // to actually update the css, you have to quit wxt entirely and start a new dev server
 
 export default defineContentScript({
